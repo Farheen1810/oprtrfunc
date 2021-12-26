fdc69285dfe14e8783613e9d5a25838ba44d8392
Farheen1810 <90109907+Farheen1810@users.noreply.github.com>
2021-12-26 13:50:00 +0530

    Add files via upload

---
+++ oprtorfunctn.java
@@ -0,0 +1,23 @@
+public class oprtorfunctn {
+    public static void oprtr()
+    {
+        int num1=10,num2=20;
+        int sum,sub,mul,div,mod;
+        System.out.println("Num1="+num1);
+        System.out.println("Num2="+num2);
+        sum=num1+num2; 
+        sub=num2-num1;
+        mul=num1*num2;
+        div=num2/num1;//it gives the quotient//
+        mod=num2%num1;//it gives the remainder//
+        System.out.println("sum="+sum);
+        System.out.println("sub="+sub);
+        System.out.println("mul="+mul);
+        System.out.println("div="+div);
+        System.out.println("mod="+mod);
+    }
+    public static void main(String[] args) {
+        oprtr();
+    }
+    
+}
